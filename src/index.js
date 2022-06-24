62bc1503ab6b28f46bf07d9d63a5d48ef8117415
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-06-24 21:45:11 +0300

    СделанаВ клалькуляторе разрешен ввод только цифр

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
+import calc from "./modules/calc";
 
 timer('26 june 2022');
 menu();
 modal();
+calc();
