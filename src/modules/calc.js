aea1c6fee72592e2acc3034626d924c6c935c418
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-06-27 15:53:13 +0300

    сделано усложненное задание 21 урока

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -6,34 +6,45 @@ const calc = () => {
     const userMessage = document.querySelector('#form2-message');
 
 
-
+    const blur = (e) => {
+        e.target.value = e.target.value.replace(/^[\s\-]+/g, '');
+        e.target.value = e.target.value.replace(/[\s\-]+$/g, '');
+        e.target.value = e.target.value.replace(/\s{2,}/g, ' ');
+        e.target.value = e.target.value.replace(/\-{2,}/g, '-');
+        e.target.value = e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1).toLowerCase();
+    };
     textCalcBlock.forEach(textBlock => {
         textBlock.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/\D+/gi, '');
         });
+
     });
+
 
     formName.forEach(formName => {
         formName.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^а-яА-Я\-\s]+/, '');
         });
+        formName.addEventListener('blur', blur);
     });
 
     userMessage.addEventListener('input', (e) => {
         e.target.value = e.target.value.replace(/[^а-яА-Я\-\s]+/, '');
     });
-
+    userMessage.addEventListener('blur', blur);
 
     formEmail.forEach(email => {
         email.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^\w\@\-\_\.\!\~\*\']+/, '');
         });
+        email.addEventListener('blur', blur);
     });
 
     formPhone.forEach(phone => {
         phone.addEventListener('input', (e) => {
             e.target.value = e.target.value.replace(/[^\d\(\)\-]+/, '');
         });
+        phone.addEventListener('blur', blur);
     });
 
 };
