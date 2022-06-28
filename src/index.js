348f17bdbfcd89d5bb6fd75faa55635bac377ffb
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-06-28 17:16:18 +0300

    add slider.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import calc from "./modules/calc";
 import tabs from "./modules/tabs";
+import slider from "./modules/slider";
 
-timer('26 june 2022');
+
+
+
+timer('01 july 2022');
 menu();
 modal();
 calc();
 tabs();
+slider();
