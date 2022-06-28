3ca5201299c6b6ba66845ceb353f27710ab7cb51
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-06-28 12:03:17 +0300

    delete comment

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,6 +1,6 @@
 const menu = () => {
 
-    
+
     const menu = document.querySelector('menu');
     const mainBlock = document.querySelector('main');
 
@@ -24,7 +24,7 @@ const menu = () => {
                 behavior: 'smooth',
                 block: 'start'
             });
-        //Здесь нужно проверять принадлежит ли клик блоку mainBlock, а не существует ли класс
+
         } else if (e.target.closest('a[href*="#"]') && mainBlock.contains(e.target)) {
             e.preventDefault();
             const blockId = e.target.closest('a[href*="#"]').getAttribute('href');
@@ -32,7 +32,7 @@ const menu = () => {
                 behavior: 'smooth',
                 block: 'start'
             });
-        //Это условие нужно перенести под конец, потому что на нём завершаются проверки, так как любой клик будет соответсовать не открытому меню
+
         } else if (menu.classList.contains('active-menu')) {
             menu.classList.remove('active-menu');
         }
@@ -43,40 +43,3 @@ const menu = () => {
 
 export default menu;
 
-  
-    // scrollBtn.addEventListener('click', (e) => {
-    //     e.preventDefault();
-    //     const blockID = scrollBtn.getAttribute('href').substr(1);
-
-    //     document.getElementById(blockID).scrollIntoView({
-    //         behavior: 'smooth',
-    //         block: 'start'
-    //     });
-    // });
-
-
-    // anchors.forEach(anchor => {
-    //     anchor.addEventListener('click', (e) => {
-    //         e.preventDefault();
-    //         const blockID = anchor.getAttribute('href').substr(1);
-
-    //         document.getElementById(blockID).scrollIntoView({
-    //             behavior: 'smooth',
-    //             block: 'start'
-    //         });
-    //     });
-    // });
-
-
-    
-    // menuBtn.addEventListener("click", handleMenu);
-
-    // menu.addEventListener("click", (e) => {
-    //     console.log(e.target);
-    //     if (
-    //         e.target.closest(`a`) ||
-    //         e.target.classList.contains("close-btn")
-    //     ) {
-    //         handleMenu();
-    //     }
-    // });
