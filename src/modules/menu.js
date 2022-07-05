de7aec5a0aa85db09a486194781ff09e4dbbc31f
Lena Ksenofontova <lena.ksenofontova@mail.ru>
2022-07-05 21:41:27 +0300

    fix comments for project

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -32,7 +32,7 @@ const menu = () => {
                 block: 'start'
             });
 
-        } else if (!e.target.closest('.active-menu')) {
+        } else if (!e.target.contains('.active-menu')) {
             menu.classList.remove('active-menu');
         }
     });
